15ddf21a4fab290545d8a86e509f89e7c827721f
archerpmorgan <archerpmorgan@email.arizona.edu>
2019-04-20 10:02:01 -0700

    XO

---
+++ initdisk.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+
+Idea is to write initial root directory to disk file. Perhaps we don't need to do this (maybe FUSE does it...? OR
+maybe its already there and all zeros is just the reeasonable start state for the root struct) but for testing it 
+will be handy anyway, for now
+
+UPDATE
+
+root directory struct is all zeros for empty root. So the disk is initialized with the root struct already in there.
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <ctype.h>
+
+//size of a disk block
+#define	BLOCK_SIZE 512
+
+//we'll use 8.3 filenames
+#define	MAX_FILENAME 8
+#define	MAX_EXTENSION 3
+
+//How many files can there be in one directory?
+#define MAX_FILES_IN_DIR (BLOCK_SIZE - sizeof(int)) / ((MAX_FILENAME + 1) + (MAX_EXTENSION + 1) + sizeof(size_t) + sizeof(long))
+
+//The attribute packed means to not align these things
+struct csc452_directory_entry
+{
+	int nFiles;	//How many files are in this directory.
+				//Needs to be less than MAX_FILES_IN_DIR
+
+	struct csc452_file_directory
+	{
+		char fname[MAX_FILENAME + 1];	//filename (plus space for nul)
+		char fext[MAX_EXTENSION + 1];	//extension (plus space for nul)
+		size_t fsize;					//file size
+		long nStartBlock;				//where the first block is on disk
+	} __attribute__((packed)) files[MAX_FILES_IN_DIR];	//There is an array of these
+
+	//This is some space to get this to be exactly the size of the disk block.
+	//Don't use it for anything.  
+	char padding[BLOCK_SIZE - MAX_FILES_IN_DIR * sizeof(struct csc452_file_directory) - sizeof(int)];
+} ;
+
+typedef struct csc452_root_directory csc452_root_directory;
+
+#define MAX_DIRS_IN_ROOT (BLOCK_SIZE - sizeof(int)) / ((MAX_FILENAME + 1) + sizeof(long))
+
+struct csc452_root_directory
+{
+	int nDirectories;	//How many subdirectories are in the root
+						//Needs to be less than MAX_DIRS_IN_ROOT
+	struct csc452_directory
+	{
+		char dname[MAX_FILENAME + 1];	//directory name (plus space for nul)
+		long nStartBlock;				//where the directory block is on disk
+	} __attribute__((packed)) directories[MAX_DIRS_IN_ROOT];	//There is an array of these
+
+	//This is some space to get this to be exactly the size of the disk block.
+	//Don't use it for anything.  
+	char padding[BLOCK_SIZE - MAX_DIRS_IN_ROOT * sizeof(struct csc452_directory) - sizeof(int)];
+} ;
+
+typedef struct csc452_directory_entry csc452_directory_entry;
+
+//How much data can one block hold?
+#define	MAX_DATA_IN_BLOCK (BLOCK_SIZE)
+
+struct csc452_disk_block
+{
+	//All of the space in the block can be used for actual data
+	//storage.
+	char data[MAX_DATA_IN_BLOCK];
+};
+
+typedef struct csc452_disk_block csc452_disk_block;
+
+
+
+
+
+
+
+
+
+int main() {
+
+	FILE * fp;
+   	fp = fopen (".disk", "ab+");
+
+   	struct csc452_root_directory root;
+   	root.nDirectories = 0;
+
+	fseek(fp, 0, SEEK_SET);
+	fwrite(&root, sizeof(csc452_root_directory), 1, fp);   	
+
+  	fclose(fp);
+
+}
